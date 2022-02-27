0b2a8c0b496dd4e1534c0fedfe8cb48cee6db641
Ohta Akira <waterlow3442@gmail.com>
2022-02-27 11:16:40 +0900

    デプロイ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
   def health_check
     render plain: 'ok'
   end
+
+  def sample
+    render plain: 'v1'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
   get 'health_check', to: 'application#health_check'
+  get 'sample', to: 'application#sample'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
